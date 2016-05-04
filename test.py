adef2f8b3ed989ae24feb1264f5d0de4f426ee7a
lukanin-artem <lukanin@phystech.edu>
2016-05-04 22:26:17 +0000

    effort

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def test():
 	result = 0
 	with open("answer.txt", "r") as f:
 		result = float(f.read())
-		if result > 0.5:
-			return True
-		else:
-			return False
+	if result > 0.5:
+		return True
+	else:
+		return False
